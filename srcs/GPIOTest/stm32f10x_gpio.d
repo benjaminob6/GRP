e93e25ff0eb847199a0a283ffa7fe44a8847325d
Ben O'Brien <benjaminob6@gmail.com>
2013-04-21 14:39:47 -0400

    Cleaned up the Repo

--- srcs/GPIOTest/stm32f10x_gpio.d
+++
@@ -1,8 +0,0 @@
-stm32f10x_gpio.o: \
- /home/zagi/stm32/stm32vl_lib/Libraries/STM32F10x_StdPeriph_Driver/src/stm32f10x_gpio.c \
- /home/zagi/stm32/stm32vl_lib/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_gpio.h \
- /home/zagi/stm32/stm32vl_lib/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/stm32f10x.h \
- /home/zagi/stm32/stm32vl_lib/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
- /home/zagi/stm32/stm32vl_lib/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.h \
- ../stm32f10x_conf.h \
- /home/zagi/stm32/stm32vl_lib/Libraries/STM32F10x_StdPeriph_Driver/inc/stm32f10x_rcc.h
